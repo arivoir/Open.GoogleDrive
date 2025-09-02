a79f9e60bba41714b065e248f52a2879f67b2341
Alvaro Rivoir <arivoir@gmail.com>
2025-09-02 00:26:06 -0300

    improvements

---
+++ Open.GoogleDrive.Test/Tests.cs
@@ -0,0 +1,34 @@
+﻿using DotNetEnv;
+using Open.GoogleDrive;
+
+namespace Open.GoogleDrive.Test;
+
+public class Tests
+{
+    private string _accessToken;
+    private string _rootFolderId;
+
+    [SetUp]
+    public async Task Setup()
+    {
+        Env.Load();
+        var clientId = Environment.GetEnvironmentVariable("CLIENT_ID")!;
+        var refreshToken = Environment.GetEnvironmentVariable("REFRESH_TOKEN")!;
+        var token = await GoogleDriveClient.RefreshAccessTokenAsync(refreshToken, clientId, "", CancellationToken.None);
+        _accessToken = token.AccessToken;
+        var client = new GoogleDriveClient(_accessToken);
+        var rootFolderName = Guid.NewGuid().ToString();
+        _rootFolderId = rootFolderName;
+        await client.InsertFileAsync(new File
+        {
+            Name = rootFolderName,
+            MimeType = "application/vnd.google-apps.folder"
+        }, null, CancellationToken.None);
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
